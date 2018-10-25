61041316fec82b59aa6e450eba445be8a5a814b3
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:48:13 +0300

    flask

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+
+    return render_template("home.html")
+
+@app.route('/shop')
+def shop():
+	product=["cookies", "cake" ,"pizza"]
+	return render_template("shop.html",
+		products=product)
 
 if __name__ == '__main__':
    app.run(debug = True)
